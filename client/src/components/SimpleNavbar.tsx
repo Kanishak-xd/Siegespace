e8a34af04400440ef214af6fa7b629865d320051
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-25 05:20:15 +0530

    added gradient text component and styling to navbar component

--- client/src/components/SimpleNavbar.tsx
+++ client/src/components/SimpleNavbar.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { IconButton, Typography, Collapse, Navbar } from "@material-tailwind/react";
 import { Menu, Xmark } from "iconoir-react";
+import GradientText from './GradientText'
 
 const LINKS = [
   {
@@ -23,7 +24,7 @@ const LINKS = [
 
 function NavList() {
   return (
-    <ul className="m-2 flex flex-col gap-x-3 gap-y-1 lg:m-0 lg:flex-row lg:items-center">
+    <ul className="m-2 flex flex-col gap-x-8 gap-y-1 lg:m-0 lg:flex-row lg:items-center">
       {LINKS.map(({ title, href }) => (
         <li key={title}>
           <Typography as="a" href={href} type="small" className="p-1 hover:text-primary">
@@ -46,15 +47,19 @@ export default function SimpleNavbar() {
   }, []);
 
   return (
-    <Navbar className="mx-auto w-full max-w-screen-xl">
+    <Navbar className="mx-auto w-full max-w-screen-xl z-10 pt-5">
       <div className="flex items-center">
-        <Typography as="a" href="#" type="small" className="mx-2 block py-1 font-semibold">
-          Siegespace
+        <Typography as="a" href="#" type="small" className="mx-2 block py-1">
+          <GradientText colors={["#DE97F9", "#9157FF"]}
+            animationSpeed={1} showBorder={false} className="custom-class text-2xl"
+          >
+            Siegespace
+          </GradientText>
         </Typography>
-        <div className="hidden lg:ml-auto lg:mr-2 lg:block">
+        <div className="hidden lg:ml-auto lg:mr-2 lg:block font-semibold text-xl">
           <NavList />
         </div>
-        <IconButton size="sm" variant="ghost" color="secondary" onClick={() => setOpenNav(!openNav)} className="ml-auto grid lg:hidden">
+        <IconButton size="sm" variant="ghost" color="secondary" onClick={() => setOpenNav(!openNav)} className="ml-auto grid lg:hidden mr-3 mt-1">
           {openNav ? (
             <Xmark className="h-4 w-4" />
           ) : (
