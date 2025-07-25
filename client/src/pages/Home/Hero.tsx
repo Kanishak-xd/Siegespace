eda84cbb2ba495728a382c5d42ccabcb4b19f679
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-25 14:45:01 +0530

    made hero section responsive

--- client/src/pages/Home/Hero.tsx
+++ client/src/pages/Home/Hero.tsx
@@ -1,29 +1,29 @@
 export default function Hero() {
     return (
-      <section className="z-0 mt-10">
-          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-              <div className="grid grid-cols-1 md:grid-cols-2 md:items-center md:gap-8">
-                <div className="max-w-lg md:max-w-none ">
-                    <p className="font-semibold text-xl sm:text-xs md:text-xl  lg:text-2xl whitespace-nowrap">
-                        I'm Kanishak, an aspiring <span className="intro-gradient">web developer </span>
-                    </p>
-                    <p className="font-semibold text-xl sm:text-xs md:text-xl lg:text-2xl whitespace-nowrap">
-                        with a knack for UI/UX & graphic design
-                    </p>
+      <section className="z-0 mt-15 px-auto xl:px-auto md:px-15 sm:px-auto">
+            <div className="mx-auto max-w-screen-lg px-4 py-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 sm:items-center sm:gap-8">
+                    <div className="max-w-lg md:max-w-none flex flex-col justify-center items-start">
+                        <p className="font-semibold sm:text-md md:text-xl  lg:text-2xl whitespace-nowrap">
+                            I'm Kanishak, an aspiring <span className="intro-gradient">web developer </span>
+                        </p>
+                        <p className="font-semibold sm:text-md md:text-xl lg:text-2xl whitespace-nowrap">
+                            with a passion for UI/UX & graphic design
+                        </p>
 
-                    <p className="mt-4 text-xl sm:text-xs md:text-xl lg:text-2xl whitespace-nowrap">
-                        Currently studying at NIIT University<br/>
-                        Graduated from DAV Public School
-                    </p>
+                        <p className="mt-4 sm:text-md md:text-lg lg:text-[1.35rem] whitespace-nowrap text-neutral-500">
+                            Currently studying at <span className="text-black">NIIT University</span><br/>
+                            Graduated from <span className="text-black">DAV Public School</span>
+                        </p>
+                    </div>
+    
+                    <div className="flex justify-center items-center">
+                        <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1753401988/boy_h9zs9c.png"
+                            className="xl:w-70 xl:h-70 lg:w-60 lg:h-60 md:w-50 md:h-50 w-50 h-50 hidden sm:block" alt="website-boy-mascot"
+                        />
+                    </div>
                 </div>
-  
-                <div className="flex justify-center items-center">
-                    <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1753401988/boy_h9zs9c.png"
-                        className="w-75 h-75" alt="website-boy-mascot"
-                    />
-                </div>
-              </div>
-          </div>
+            </div>
       </section>
     )
 }
