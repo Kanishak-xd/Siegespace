d27645188beac7a5199f49eca885937b6b0a3dd0
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-27 14:13:14 +0530

    made project cards dynamic

--- client/src/pages/Home/ProjectsSection/ProjectCard.tsx
+++ client/src/pages/Home/ProjectsSection/ProjectCard.tsx
@@ -1,16 +1,29 @@
-export default function ProjectCard() {
+interface ProjectCardData {
+  id: number;
+  heading: string;
+  subheading: string;
+  description: string;
+  thumbnail: string;
+}
+
+export default function ProjectCard({ projectCardData }: { projectCardData: ProjectCardData }) {
+  const { heading, subheading, description, thumbnail } = projectCardData;
   return (
     <>
         <div className="max-w-full p-4 border-1 border-black rounded-xl hover:scale-101 hover:cursor-pointer shadow-lg dark:bg-neutral-50 dark:text-neutral-900">
-            <img src="" alt="project-thumbnail" 
+            <img src={ thumbnail } alt="project-thumbnail" 
               className="object-cover object-center w-full rounded-xl xl:h-82 lg:h-62 md:h-62 sm:h-62 h-42 dark:bg-gray-500" 
             />
             <div className="mt-1 mb-1">
-              <h2 className="text-2xl sm:text-2xl md:text-[1.6rem] xl:text-[2rem] font-bold tracking-wide">PROJECT HEADING</h2>
-              <h3 className="text-xl sm:text-xl md:text-[1.4rem] xl:text-[1.5rem] text-neutral-800 font-semibold tracking-wide">Project subheading</h3>
+              <h2 className="text-2xl sm:text-2xl md:text-[1.6rem] xl:text-[2rem] font-bold tracking-wide">
+                { heading }
+              </h2>
+              <h3 className="text-xl sm:text-xl md:text-[1.4rem] xl:text-[1.5rem] text-neutral-800 font-semibold tracking-wide">
+                { subheading }
+              </h3>
             </div>
-            <p className="dark:text-neutral-600 text-1 sm:text-1 md:text-1 xl:text-xl">Some additional text about the project, maximum two lines,
-              otherwise it will overflow
+            <p className="dark:text-neutral-600 text-1 sm:text-1 md:text-1 xl:text-xl">
+              { description }
             </p>
         </div>
     </>
