a275d919107ed48a632632cf42d8f42b3e705ef2
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-27 15:57:24 +0530

    updated responsiveness for hero and projects

--- client/src/pages/Home/ProjectsSection/ProjectCard.tsx
+++ client/src/pages/Home/ProjectsSection/ProjectCard.tsx
@@ -16,14 +16,17 @@ export default function ProjectCard({ projectCardData }: { projectCardData: Proj
               className="object-cover object-center w-full rounded-xl xl:h-82 lg:h-62 md:h-62 sm:h-62 h-42 dark:bg-gray-500" 
             />
             <div className="mt-1 mb-1">
-              <p className="text-[1.3rem] sm:text-[1.3rem] md:text-[1.6rem] xl:text-[2rem] font-bold tracking-wide">
-                { heading }
-              </p>
+              <div className="flex justify-start items-center">
+                <p className="text-[1.3rem] sm:text-[1.3rem] md:text-[1.6rem] xl:text-[2rem] font-bold tracking-wide">
+                  { heading }
+                </p>
+                <img src="https://cdn-icons-png.flaticon.com/512/60/60947.png" alt="top-right-icon" className="h-3 w-3 ml-1 md:h-3 md:w-3 xl:h-4 xl:w-4" />
+              </div>
               <p className="text-[0.9rem] sm:text-[0.9rem] md:text-[1.2rem] xl:text-[1.5rem] text-neutral-800 font-semibold tracking-wide">
                 { subheading }
               </p>
             </div>
-            <p className="dark:text-neutral-600 text-[0.8rem] sm:text-[0.8rem] md:text-1 xl:text-xl">
+            <p className="dark:text-neutral-600 text-[0.8rem] sm:text-[0.8rem] md:text-1 xl:text-[1.15rem]">
               { description }
             </p>
         </a>
--- client/src/pages/Home/ProjectsSection/Projects.tsx
+++ client/src/pages/Home/ProjectsSection/Projects.tsx
@@ -3,9 +3,9 @@ import ProjectCardData from "./Projects.json";
 
 export default function Projects() {
   return (
-    <section className="z-0 mt-15 px-auto xl:px-auto md:px-15 sm:px-auto">
+    <section className="z-0 xl:mt-15 md:mt-10 sm:mt-0 px-auto xl:px-auto md:px-15 sm:px-auto">
         <div className="mx-auto max-w-screen-lg py-8 p-5 sm:p-10 md:p-5 lg:p-5 xl:p-5">
-            <p className="text-3xl font-bold">Featured work</p>
+            <p className="text-xl sm:text-xl md:text-2xl xl:text-3xl font-bold">Featured work</p>
             <div className="mt-4 grid grid-cols-1 grid-rows-4 sm:grid-cols-1 sm:grid-rows-4 md:grid-cols-2 md:grid-rows-2 xl:grid-cols-2 xl:grid-rows-2 gap-8">
                 {ProjectCardData.map((project) => (
                   <ProjectCard key={ project.id } projectCardData={ project } />
